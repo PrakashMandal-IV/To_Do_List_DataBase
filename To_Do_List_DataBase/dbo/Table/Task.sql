331af026772c9d6ebc901036cd2cfeb12615ef55
Prakash <prakashmandal.iv@gmail.com>
2022-05-19 02:51:29 +0530

    Add project files.

---
+++ To_Do_List_DataBase/dbo/Table/Task.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Task] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Title]       NVARCHAR (MAX) NOT NULL,
+    [WhatToDo]    NVARCHAR (MAX) NOT NULL,
+    [CreatedAt]   DATETIME2 (7)  NOT NULL,
+    [IsCompleted] BIT            NOT NULL,
+    [UserId]      INT            NOT NULL,
+    CONSTRAINT [PK_Task] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Task_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Task_UserId]
+    ON [dbo].[Task]([UserId] ASC);
+
---
+++ To_Do_List_DataBase/dbo/Table/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]  NVARCHAR (MAX) NOT NULL,
+    [FirstName] NVARCHAR (MAX) NOT NULL,
+    [LastName]  NVARCHAR (MAX) NOT NULL,
+    [Password]  NVARCHAR (MAX) NOT NULL,
+    [CreatedAt] DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
